8f8f4a32b87e140f4a79a6415a6f1cce79bbbf3f
t-koga <t.koga0410@gmail.com>
2019-04-06 01:46:07 +0900

    test preparation 2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,21 @@ module ApplicationHelper
   def nl2br(str)
     h(str).gsub(/\R/, "<br>")
   end
+
+  def index_display(models)
+    unit =
+      case models
+      when @rooms
+        "部屋"
+      when @users
+        "人"
+      when @articles
+        "件"
+      else
+        "件"
+      end
+    unless models.total_count == 0
+      "#{models.page(params[:page]).offset_value + 1} - #{models.total_count - models.page(params[:page]).offset_value} / #{models.total_count}#{unit}中"
+    end
+  end
 end
